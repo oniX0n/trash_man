837ff289b6d9714a5cb16f1232d2cca30caaa065
Theo Müller <theopaul2001@gmx.de>
2025-12-04 17:25:47 +0100

    rewrite maid

--- src/maid/main.rs
+++ src/maid/main.rs
@@ -1,37 +1,39 @@
 use std::path::Path;
 use std::env;
-
+use std::io;
 
 const TRASH_PATH: &str = "/home/theo/test";
 
 
-fn main()  {
+fn main() -> io::Result<()> {
 
     let args: Vec<String> = env::args().collect();
 
-    let path_string = &args[1];
-    let path = Path::new(path_string);
+    let path_arg = &args[1];
+    let path = Path::new(path_arg);
 
     match path.try_exists() {
         Ok(false) => {
-            eprintln!("The File or Folder doesn't exist");
-            return;
+            return Err(io::Error::new(io::ErrorKind::NotFound ,"The file or folder dosen't exist"));
         },
         Err(error) => {
-            eprintln!("Can't access path: {:?}", error);
-            return
+            return Err(error);
         },
         _ => (),
     }
 
-    let absolute_path = std::path::absolute(path).expect("Path is invalid");
+    let name = path
+        .file_name().ok_or(io::Error::other("Can't get the file or folder name"))?
+        .to_str().ok_or(io::Error::other("Can't convert OS String"))?;
 
+    let time = chrono::Local::now();
 
-    let absolute_path_str = absolute_path.to_str().expect("Not unicode");
-    let target_string = format!("{}{}", TRASH_PATH, absolute_path_str);
-    let target_path = Path::new(&target_string);
+    let target_str = format!("{}/{}-{}", TRASH_PATH, name, time.format("%d-%m-%H:%M:%S"));
+    let target_path = Path::new(&target_str);
 
-    std::fs::create_dir_all(target_path.parent().unwrap()).expect("Coudnt");
-    std::fs::rename(path, target_path).expect("Yo dawg aint working");
+    std::fs::create_dir_all(target_path.parent().ok_or(io::Error::other("Couldn't get parent of path"))?)?;
+    std::fs::rename(path, target_path)?;
+
     println!("{:?} -> {:?}", path, &target_path);
+    Ok(())
 }
